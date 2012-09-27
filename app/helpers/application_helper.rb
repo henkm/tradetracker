434901577eb13724e530332f90b6df68656d37fd
Henk Meijer <meijerhenk@gmail.com>
2012-09-27 16:18:43 +0200

    first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,8 @@ module ApplicationHelper
     if banner
       old_affiliate_code = url.split('_&r=').first.split('_').last
       url.gsub(old_affiliate_code.to_s, banner.affiliate.tradetracker_id.to_s)
+      # http://www.concertgebouw.nl/kaarten/index.php?tt=8522_319827_40405_&r=http%3A%2F%2Fwww.concertgebouw.nl%2Fx%2F32185
+
     else
       url
     end
