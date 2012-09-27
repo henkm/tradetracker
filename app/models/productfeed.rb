70b90f5468d2c078fefde6773cdee50def6fa1b1
Henk Meijer <meijerhenk@gmail.com>
2012-09-27 16:58:49 +0200

    update task

--- app/models/productfeed.rb
+++ app/models/productfeed.rb
@@ -21,40 +21,42 @@ class Productfeed < ActiveRecord::Base
     if Rails.env == 'development'
       url = url + "&limit=250"
     end
-    xml = open(url).read unless xml.present?
-    doc = Nokogiri::XML(xml)
-    doc.xpath('//product').each do |item|      
-      original_id = get_value(item, original_id_field, original_id_attribute)
-      product = products.find_or_initialize_by_original_id(original_id)
-      product.title = get_value(item, title_field)
-      product.image = get_value(item, image_field, image_attribute, image_value)
-      product.description = get_value(item, description_field)
-      product.url = get_value(item, url_field, url_attribute)
-      product.old_price = get_value(item, old_price_field, old_price_attribute)
-      product.new_price = get_value(item, new_price_field, new_price_attribute)
-      product.currency = get_value(item, currency_field, currency_attribute)
-      [1,2,3].each do |i|
-        val = get_extra_value(item, i)
-        if send("extra_info#{i}_type") == "Datum"
-          begin
-            date = Date.parse(val) if val.present?
-            val = I18n.l(date) if date
-          rescue
-            logger.info "invalid date"
+    if xml_url.present?
+      xml = open(url).read unless xml.present?
+      doc = Nokogiri::XML(xml)
+      doc.xpath('//product').each do |item|      
+        original_id = get_value(item, original_id_field, original_id_attribute)
+        product = products.find_or_initialize_by_original_id(original_id)
+        product.title = get_value(item, title_field)
+        product.image = get_value(item, image_field, image_attribute, image_value)
+        product.description = get_value(item, description_field)
+        product.url = get_value(item, url_field, url_attribute)
+        product.old_price = get_value(item, old_price_field, old_price_attribute)
+        product.new_price = get_value(item, new_price_field, new_price_attribute)
+        product.currency = get_value(item, currency_field, currency_attribute)
+        [1,2,3].each do |i|
+          val = get_extra_value(item, i)
+          if send("extra_info#{i}_type") == "Datum"
+            begin
+              date = Date.parse(val) if val.present?
+              val = I18n.l(date) if date
+            rescue
+              logger.info "invalid date"
+            end
           end
+          product["extra_info#{i}"] = val
         end
-        product["extra_info#{i}"] = val
-      end
-      product.update_version = time_before_update
-      logger.info product.inspect
-      unless product.title.present? && product.description.present?
-        logger.info "Destroy product"
-        product.destroy
-      else
-        logger.info "Saving product"
-        product.save
-      end
+        product.update_version = time_before_update
+        logger.info product.inspect
+        unless product.title.present? && product.description.present?
+          logger.info "Destroy product"
+          product.destroy
+        else
+          logger.info "Saving product"
+          product.save
+        end
 
+      end
     end
     logger.info "Destroying old products"
     products.where("update_version != '#{time_before_update}'").destroy_all
