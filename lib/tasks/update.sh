b55877f88c5fa0ca5a33bcd04d35b54e4445891e
Henk Meijer <meijerhenk@gmail.com>
2012-10-01 15:51:39 +0200

    banners

--- lib/tasks/update.sh
+++ lib/tasks/update.sh
@@ -2,4 +2,4 @@ RAILS_ENV=production; source /usr/local/rvm/scripts/rvm;
 
 # source /usr/local/rvm/scripts/rvm 
 cd /home/.sites/48/site10/web/current
-bundle exec rake update_products RAILS_ENV=production
+/usr/local/rvm/gems/ruby-1.9.3-p194/bin/bundle exec rake update_products RAILS_ENV=production
