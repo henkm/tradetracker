69831b4922963eb175b7dcce44a48c4de3d65789
Henk Meijer <meijerhenk@gmail.com>
2012-10-01 16:05:42 +0200

    banners

--- lib/tasks/update.sh
+++ lib/tasks/update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-source /usr/local/rvm/scripts/rvm
+
+# source /usr/local/rvm/scripts/rvm
 
 # source /usr/local/rvm/scripts/rvm 
 cd /home/.sites/48/site10/web/current
-bundle exec rake update_products RAILS_ENV=production
+./bin/rake update_products RAILS_ENV=production
